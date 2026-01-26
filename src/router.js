6c67096615e9f69c4edc3d7a71954e23de230221
Mian Ammar Salar <miantesting82@gmail.com>
2026-01-26 09:27:16 +0500

    Dynamic Page title done

--- src/router.js
+++ src/router.js
@@ -16,60 +16,98 @@ const routes = [
         path: "/",
         name: "HomeComponent",
         component: HomeComponent,
+        meta:{
+            title:"Cine List | Home"
+        }
     },
     {
         path: "/movies",
         name: "MoviesComponent",
         component: MoviesComponent,
+        meta:{
+            title:"Cine List | Movies"
+        }
     },
     {
         path: "/tv-shows",
         name: "TVShowsComponent",
         component: TvShowsComponent,
+        meta:{
+            title:"Cine List | TV Shows"
+        }
     },
     {
         path: "/watchlist",
         name: "WatchlistComponent",
         component: WatchlistComponent,
+        meta:{
+            title:"Cine List | Watch List"
+        }
     },
     {
         path: "/login",
         name: "LoginComponent",
         component: LoginComponent,
+        meta:{
+            title:"Cine List | Login"
+        }
     },
     {
         path: "/register",
         name: "RegisterComponent",
         component: RegisterComponent,
+        meta:{
+            title:"Cine List | Register"
+        }
     },
     {
         path: "/contact",
         name: "ContactComponent",
         component: ContactComponent,
+        meta:{
+            title:"Cine List | Contact"
+        }
     },
     {
         path: "/about",
         name: "AboutComponent",
         component: AboutComponent,
+        meta:{
+            title:"Cine List | About"
+        }
     },
     {
         path: "/privacy-policy",
         name: "PrivacyComponent",
         component: PrivacyComponent,
+        meta:{
+            title:"Cine List | Privacy Policy"
+        }
     },
     {
         path: "/terms-and-conditions",
         name: "TermsAndConditionComponent",
-        component: TermAndConditionComponent
+        component: TermAndConditionComponent,
+        meta:{
+            title:"Cine List | Terms And Condition"
+        }
     },
     {
         path: '/:pathMatch(.*)*',
         name: 'NotFoundComponent',
-        component: NotFoundComponent
+        component: NotFoundComponent,
+        meta:{
+            title:"Cine List | 404 Not Found"
+        }
     }
 ]
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title}`;
+    next();
 })
 export default router;
